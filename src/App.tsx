b1c5afcb3e474621ae6970de02a223a906eb876a
gsdeoliveira <gabriielsousa2002@gmail.com>
2025-09-06 19:34:09 -0300

    feat: init project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+export const App = () => {
+  return (
+    <>
+      <h1>Chronos Pomodoro</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+        optio praesentium itaque non voluptatum mollitia eius, id consequatur
+        maiores. Consequuntur nam voluptatum quia, perspiciatis omnis velit aut
+        laudantium sequi neque!
+      </p>
+    </>
+  )
+}
