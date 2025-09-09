75cef28d1f6b767713ebd16750263ef39fc2ebd8
gsdeoliveira <gabriielsousa2002@gmail.com>
2025-09-09 20:34:25 -0300

    feat: logo, menu and countdown components

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
-import './styles/theme.css'
-import './styles/global.css'
 import { Container } from './components/Container'
 import { Heading } from './components/Heading'
+import { Logo } from './components/Logo'
+
+import './styles/theme.css'
+import './styles/global.css'
+import { Menu } from './components/Menu'
+import { CountDown } from './components/CountDown'
 
 export const App = () => {
   return (
     <>
       <Container>
-        <Heading>Chronos Pomodoro</Heading>
+        <Heading>
+          <Logo />
+        </Heading>
       </Container>
       <Container>
-        <section>MENU</section>
+        <Menu />
       </Container>
       <Container>
-        <section>FORM</section>
+        <CountDown />
       </Container>
       <Container>
         <section>FOOTER</section>
