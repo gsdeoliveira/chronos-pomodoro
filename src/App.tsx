f978a262deb60d03b233146566e83c2de9fcdd69
gsdeoliveira <gabriielsousa2002@gmail.com>
2026-01-04 17:10:06 -0300

    feat: create TaskContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import { Home } from './pages/Home'
 import './styles/theme.css'
 import './styles/global.css'
 
+import { Home } from './pages/Home'
+import { TaskContextProvider } from './contexts/TaskContext/TaskContextProvider'
+
 export const App = () => {
-  return <Home />
+  return (
+    <TaskContextProvider>
+      <Home />
+    </TaskContextProvider>
+  )
 }
