090db179a4e5886d04fa112abbdeddfbb2e78cac
gsdeoliveira <gabriielsousa2002@gmail.com>
2026-01-09 21:51:55 -0300

    feat: create function handleCreateNewTask

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -4,8 +4,13 @@ import { DefaultButton } from '../DefaultButton'
 import { DefaultInput } from '../DefaultInput'
 
 export const MainForm = () => {
+  function handleCreateNewTask(event: React.FormEvent) {
+    event.preventDefault()
+    console.log('Criando nova tarefa')
+  }
+
   return (
-    <form className='form' action=''>
+    <form onSubmit={handleCreateNewTask} className='form' action=''>
       <div className='formRow'>
         <DefaultInput labelText='task' id='meuInput' type='text' placeholder='Digite algo' />
       </div>
